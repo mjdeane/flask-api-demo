ae6217ddb99048a92da97b63ac4e0f0106d242a4
MatthewDeane <mjdeane@edu.uwaterloo.ca>
2018-01-08 10:23:14 -0500

    Now runs in Docker container

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ api.add_resource(ThingList, '/things')
 api.add_resource(Thing, '/things/<name>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)

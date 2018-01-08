7eacdd0d04972ef5d5e3c211349e408d202b99bf
Jeremy <jmills-bespoke@users.noreply.github.com>
2018-01-08 12:30:01 -0500

    Update dao.py

--- dao.py
+++ dao.py
@@ -4,7 +4,11 @@ from model import Model
 class Dao:
     def __init__(self, app):
         app.config['MONGO_DBNAME'] = 'thing_list'
+        
+        # TODO: clusert configuration is good here, but this will connect to a local mongo docker container once you build out
+        # your docker-compose file
         app.config['MONGO_URI'] = 'mongodb://admin:admin@cluster0-shard-00-00-bwkql.mongodb.net:27017,cluster0-shard-00-01-bwkql.mongodb.net:27017,cluster0-shard-00-02-bwkql.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+        
         self.mongo = PyMongo(app)
 
 
@@ -20,6 +24,8 @@ class Dao:
 
     def delete(self, name):
         item = self.mongo.db['things'].find_one_and_delete({'name':name})
+        
+        # TODO: debugging ?
         print(item)
         return item
 

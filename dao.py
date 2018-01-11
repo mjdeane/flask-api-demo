9dcf92c2540623fdf863b3663448e857ad887458
MatthewDeane <mjdeane@edu.uwaterloo.ca>
2018-01-11 08:54:40 -0500

    changed to use docker-compose with local mongo instance

--- dao.py
+++ dao.py
@@ -1,11 +1,12 @@
+import os
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson import json_util
 
-DB_URI = 'mongodb://admin:admin@cluster0-shard-00-00-bwkql.mongodb.net:27017,cluster0-shard-00-01-bwkql.mongodb.net:27017,cluster0-shard-00-02-bwkql.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+# REMOTE_DB_URI = 'mongodb://admin:admin@cluster0-shard-00-00-bwkql.mongodb.net:27017,cluster0-shard-00-01-bwkql.mongodb.net:27017,cluster0-shard-00-02-bwkql.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 DB_NAME = 'thing_list'
 COLLECTION = 'things'
-client = MongoClient(DB_URI)
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
 db = client[DB_NAME][COLLECTION]
 
 class Dao:        
